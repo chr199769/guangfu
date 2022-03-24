5f68c1606eaea76279f8855e34076db12d27d754
cuihaoran <cuihaoran.111@bytedance.com>
2022-03-24 18:09:58 +0800

    chore: cnpm

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,32 @@
 import React from 'react';
 import { Layout } from 'antd';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import './App.less';
 import LeftSider from './compoents/LeftSider';
 import DangerReport from './pages/dangerReport';
-import Login from './pages/login';
 import Monitor from './pages/monitor';
 import Platfrom from './pages/platfrom';
+import Header from './compoents/Header';
 
 const { Content, Sider } = Layout
 function App() {
   return (
     <Layout className='App'>
-      <Sider>
-        <LeftSider />
-      </Sider>
-      <Content>
-        <Routes>
-          <Route path='platform' element={<Platfrom />} />
-          <Route path='monitor' element={<Monitor />} />
-          <Route path='dangerReport' element={<DangerReport />} />
-        </Routes>
-      </Content>
+      <Layout.Header>
+        <Header />
+      </Layout.Header>
+      <Layout>
+        <Sider>
+          <LeftSider />
+        </Sider>
+        <Content>
+          <Routes>
+            <Route path='platform' element={<Platfrom />} />
+            <Route path='monitor' element={<Monitor />} />
+            <Route path='dangerReport' element={<DangerReport />} />
+          </Routes>
+        </Content>
+      </Layout>
     </Layout>
   );
 }
